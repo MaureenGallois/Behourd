f4245d617b178d5a4ef65291e639c11becacb870
nicolas.bern <nicolas.bern@epsi.fr>
2021-10-11 14:16:58 +0200

    Ajout fonction CheckFileExtension()

--- app/import_file.py
+++ app/import_file.py
@@ -1,4 +1,8 @@
 from xlrd import *
 
-def CheckFileExtension():
-    return None
+def CheckFileExtension(fileName):
+    if(fileName != None and fileName.find(".xlsx") != -1 or fileName.find(".csv") != -1):
+        return True
+    else:
+        print("Veuillez insérer un fichier .xlsx ou .csv")
+        return False
